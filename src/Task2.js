69db9acbe52a32e16be81ace50040d49eba29a80
pizarrosch <bayernsch89@gmail.com>
2023-03-01 21:03:51 +0100

    Tasks 1 and 2 (completed)

--- src/Task2.js
+++ src/Task2.js
@@ -1,6 +1,11 @@
 // Напишите функцию getPercents(percent, number), которая возвращает {percent} процентов от {number}.
 
 export default function getPercents(percent, number) {
+ if (percent > 100) {
+  return 'Please enter a number less or equal 100';
+ } else if (percent < 1) {
+  return 'Please enter a number more or equal 1';
+ }
  return number * percent / 100;
 }
 
--- src/__tests__/Task2.test.js
+++ src/__tests__/Task2.test.js
@@ -4,4 +4,20 @@ describe('A test', () => {
   it('should get a share of a number using percentage', () => {
     expect(getPercents(30, 200)).toBe(60)
   });
+
+  it('should throw error if percentage value is under 1', () => {
+    expect(getPercents(0, 200)).toBe('Please enter a number more or equal 1')
+  });
+
+  it('should throw error if percentage value is over 100', () => {
+    expect(getPercents(101, 200)).toBe('Please enter a number less or equal 100')
+  });
+
+  it('should get a share of a number using percentage beginning from 1', () => {
+    expect(getPercents(1, 200)).toBe(2)
+  });
+
+  it('should get a share of a number using percentage ending with 100', () => {
+    expect(getPercents(100, 200)).toBe(200)
+  });
 })
